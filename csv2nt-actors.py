c8149fd0e12703186b4e2bba1317675fd7669daf
creativethumbs <ticha.seth@gmail.com>
2014-07-22 17:12:42 -0400

    hopefully everything works now

--- csv2nt-actors.py
+++ csv2nt-actors.py
@@ -7,41 +7,29 @@ def writeToFile():
     global outFile
     count = 0
 
-    for line in file('actors.csv', 'r'):
+    for line in file('actors.tsv', 'r'):
         if count > 0:
-            linearray = line.split(',')
-            if len(linearray) > 3 and linearray[0][0] == "\"":
-                actorid =  linearray[2]
-                actorname = linearray[0] + linearray[1]
-                actorname = actorname[1:-1]
-            else:
-                actorid =  linearray[1]
-                actorname = linearray[0]
+            linearray = line.split('\t')
+            actorid =  re.findall(r"['\"](.*?)['\"]", linearray[1])[0]
+            actorname = linearray[0]
 
             outFile.write("<http://data.linkedmdb.org/resource/actor/" + actorid + ">" +
                 " <http://data.linkedmdb.org/resource/movie/actor_name> " +
-                "\"" + actorname + "\" .\n")
+                 actorname + " .\n")
             
         count += 1
 
     count = 0
     
-    for line in file('actors.csv', 'r'):
+    for line in file('actors.tsv', 'r'):
         if count > 0:
-            linearray = line.split(',')
-            if len(linearray) > 3 and linearray[len(linearray)-1][-1:] == "\"":
-                actorid =  linearray[1]
-                title = linearray[2] + linearray[3][:-2]
-            elif len(linearray) > 3 and linearray[0][0] == "\"":
-                actorid =  linearray[2]
-                title =  linearray[3]
-            else:
-                actorid =  linearray[1]
-                title = linearray[2][:-2]
+            linearray = line.split('\t')
+            actorid =  re.findall(r"['\"](.*?)['\"]", linearray[1])[0]
+            title = linearray[2][:-2]
 
             outFile.write("<http://data.linkedmdb.org/resource/actor/" + actorid + ">" +
                 " <http://data.linkedmdb.org/resource/movie/performance> " +
-                "\"" + title + "\" .\n")
+                title + "\" .\n")
             
         count += 1
 
--- csv2nt-directors.py
+++ csv2nt-directors.py
@@ -1,4 +1,5 @@
 import string
+import re
 
 outFile = open('directors.nt', 'w')
 
@@ -6,42 +7,29 @@ def writeToFile():
     global outFile
     count = 0
 
-    for line in file('directors.csv', 'r'):
+    for line in file('directors.tsv', 'r'):
         if count > 0:
-            linearray = line.split(',')
-            if len(linearray) > 3 and linearray[0][0] == "\"":
-                directorname = linearray[0] + linearray[1]
-                directorname = directorname[1:-1]
-                directorid = linearray[2]
-
-            else:
-                directorname =  linearray[0]
-                directorid = linearray[1]
+            linearray = line.split('\t')
+            directorname =  linearray[0]
+            directorid = re.findall(r"['\"](.*?)['\"]", linearray[1])[0]
 
             outFile.write("<http://data.linkedmdb.org/resource/director/" + directorid + ">" +
                 " <http://data.linkedmdb.org/resource/movie/director_name> " +
-                "\"" + directorname + "\" .\n")
+                directorname + " .\n")
             
         count += 1
 
     count = 0
 
-    for line in file('directors.csv', 'r'):
+    for line in file('directors.tsv', 'r'):
         if count > 0:
-            linearray = line.split(',')
-            if len(linearray) > 3 and linearray[len(linearray)-1][-1:] == "\"":
-                directorid = linearray[1]
-                title = linearray[2] + linearray[3][:-2]
-            elif len(linearray) > 3 and linearray[0][0] == "\"":
-                directorid =  linearray[2]
-                title =  linearray[3]
-            else:
-                title = linearray[2][:-2]
-                directorid = linearray[1]
+            linearray = line.split('\t')
+            title = linearray[2][:-2]
+            directorid = re.findall(r"['\"](.*?)['\"]", linearray[1])[0]
 
             outFile.write("<http://data.linkedmdb.org/resource/director/" + directorid + ">" +
                 " <http://xmlns.com/foaf/0.1/made> " +
-                "\"" + title + "\" .\n")
+                title + "\" .\n")
 
             
         count += 1
